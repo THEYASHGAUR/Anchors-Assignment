14ec1584ee01f5243ef2d8b2bd6034d8d09f93c4
Yash Gaur <yashgaur2525@gmail.com>
2023-10-17 09:06:59 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Discover Your Potential Earning</h1>
+        <h3>Turn your youtube income into lucrative income through resource sharing</h3>
+        <input
+         type="text"
+         placeholder='enter youtube video link'
+          />
+      </div>
+    </>
+  )
+}
+
+export default App;
